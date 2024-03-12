fc5f5d81c7e3f346ad9c87168e137ffd75dd224e
trungvu222 <vuthanhtrung385@gmail.com>
2024-03-12 09:13:14 +0700

    update today

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export default function Footer() {
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/nutlope"
+          href="https://twitter.com/VThnhTr36168540"
           className="group"
           aria-label="TaxPal on Twitter"
         >
